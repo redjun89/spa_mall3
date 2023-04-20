3c70b8ab0caefaaa38e73b028b88603c857accbd
redjun89 <jjune1816@gmail.com>
2023-04-20 15:30:59 +0900

    몽고디비 연결 수정

--- schemas/index.js
+++ schemas/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connect = () => {
   mongoose
     .set("strictQuery", true)
-    .connect("mongodb://localhost:27017/spa_mall")
+    .connect("mongodb://127.0.0.1:27017/spa_mall")
     .catch((err) => console.log(err));
 };
 
